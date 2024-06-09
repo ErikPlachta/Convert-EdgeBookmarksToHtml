0b49283d6c3e7de4d1c90c0e8b642f0e65e8d764
Erik Plachta <eplachta@gmail.com>
2024-06-09 09:28:15 -0400

    Update Convert-EdgeBookmarksToHtml.ps1
    Finalized parameters

--- Convert-EdgeBookmarksToHtml.ps1
+++ Convert-EdgeBookmarksToHtml.ps1
@@ -5,11 +5,17 @@ Converts Microsoft Edge bookmarks from JSON to HTML format for import into other
 .DESCRIPTION
 This PowerShell script reads the bookmarks from a Microsoft Edge JSON file and converts them into an HTML format compatible with most web browsers' bookmark import functionality.
 
-.PARAMETER JsonFilePath
-The path to the Microsoft Edge bookmarks JSON file. If not provided and PromptForPath is false, the script will use the default path for the current user's Edge bookmarks.
+.PARAMETER bookmarkPath
+The path to the Microsoft Edge bookmarks JSON file. If not provided and promptBookmarkPath is false, the script will use the default path for the current user's Edge bookmarks.
 
-.PARAMETER PromptForPath
-If set to true, the script will prompt the user with a file selection dialog to choose the bookmarks JSON file.
+.PARAMETER promptBookmarkPath
+If set to true, prompt for a bookmarks JSON file via a system window dialog.
+
+.PARAMETER outputPath
+Location where the favorites will be exported to.
+
+.PARAMETER promptOutputPath
+If set to true, prompt for save location via a system window dialog.
 
 .EXAMPLE
 Convert-EdgeBookmarksToHtml
@@ -17,12 +23,14 @@ Convert-EdgeBookmarksToHtml
 Converts the bookmarks from the default Edge bookmarks file for the current user.
 
 .EXAMPLE
-Convert-EdgeBookmarksToHtml -JsonFilePath "C:\path\to\custom\bookmarks.json"
+Convert-EdgeBookmarksToHtml -bookmarkPath "C:\path\to\custom\bookmarks.json"
 
 Converts the bookmarks from a custom JSON file specified by the user.
 
 .EXAMPLE
-Convert-EdgeBookmarksToHtml -PromptForPath $true
+Convert-EdgeBookmarksToHtml -promptBookmarkPath $true
+
+Prompts the user to select the bookmarks JSON file via a system window dialog.
 
 .NOTES
 Author:  Erik Plachta
@@ -30,70 +38,92 @@ Created: 20240609
 Version: 0.0.1
 
 #>
-function Convert-EdgeBookmarksToHtml() {
+function Convert-EdgeBookmarksToHtml {
     param (
-        [string]$JsonFilePath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Bookmarks",
-        [bool]$PromptForPath = $false
+        [string]$bookmarkPath = "$HOME\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks",
+        [bool]$promptBookmarkPath = $false,
+        [string]$outputPath = "$HOME\Desktop\bookmarks_$(((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmssZ")).html",
+        [bool]$promptOutputPath = $false
     )
 
-    # Make sure forms imported for saving and selecting file UI.
-    Add-Type -AssemblyName System.Windows.Forms
+    # If requested, prompt for the path, opens UI allowing the user to manually select a file.
+    if ($promptBookmarkPath -or $promptOutputPath) {
+        # Make sure forms are imported for saving and selecting file UI.
+        Add-Type -AssemblyName System.Windows.Forms
 
-    # If requested prompt for path, opens UI allowing user to manually select file.
-    if ($PromptForPath)
-    {
-        $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-        $openFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*"
-        $openFileDialog.ShowDialog() | Out-Null
-        $JsonFilePath = $openFileDialog.FileName
+        # Prompt for bookmark file
+        if ($promptBookmarkPath) {
+            $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+            $openFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*"
+            $openFileDialog.ShowDialog() | Out-Null
+
+            # If selected use it, otherwise default.
+            if ($openFileDialog.FileName) {
+                $bookmarkPath = $openFileDialog.FileName
+            }
+        }
+
+        # Prompt for output path
+        if ($promptOutputPath) {
+            $saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+            $saveFileDialog.Filter = "HTML File (*.html)|*.html"
+            $saveFileDialog.ShowDialog() | Out-Null
+
+            # If selected use it, otherwise default.
+            if ($saveFileDialog.FileName) {
+                $outputPath = $saveFileDialog.FileName
+            }
+        }
     }
 
-    function Process-Folder($folder) {
+    # Helper function to handle folders within bookmarks.
+    function Process-Bookmark-Folders($folder) {
         $html = "<DT><H3>$($folder.name)</H3>`n<DL><p>`n"
         foreach ($child in $folder.children) {
             if ($child.type -eq 'url') {
                 $html += "<DT><A HREF=`"$($child.url)`" ADD_DATE=`"$($child.date_added)`">$($child.name)</A>`n"
             } elseif ($child.type -eq 'folder') {
-                $html += Process-Folder $child
+                $html += Process-Bookmark-Folders $child
             }
         }
         $html += "</DL><p>`n"
         return $html
     }
 
-    # Reading JSON file
-    $jsonContent = Get-Content -Path $JsonFilePath -Raw | ConvertFrom-Json
+    # Get Edge custom JSON Bookmarks file.
+    try {
+        $jsonContent = Get-Content -Path $bookmarkPath -Raw | ConvertFrom-Json
+    } catch {
+        Write-Host "Failed to read or parse the bookmarks JSON file. Please check the path and try again."
+        return
+    }
 
     # Starting HTML file content
-    $htmlOutput = "<!DOCTYPE NETSCAPE-Bookmark-file-1>
-        <!-- This is an automatically generated file. It will be read and overwritten. DO NOT EDIT! -->
-        <META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=UTF-8'>
-        <TITLE>Bookmarks</TITLE>
-        <H1>Bookmarks</H1>
-        <DL>
-        <p>"
+    $htmlOutput = @"
+<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file. It will be read and overwritten. DO NOT EDIT! -->
+<META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=UTF-8'>
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p>
+"@
+
     foreach ($child in $jsonContent.roots.bookmark_bar.children) {
         if ($child.type -eq 'url') {
             $htmlOutput += "<DT><A HREF=`"$($child.url)`" ADD_DATE=`"$($child.date_added)`">$($child.name)</A>`n"
         } elseif ($child.type -eq 'folder') {
-            $htmlOutput += Process-Folder $child
+            $htmlOutput += Process-Bookmark-Folders $child
         }
     }
 
     $htmlOutput += "</DL><p>"
 
-    # Prompt user for output file path
-    $saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
-    $saveFileDialog.filter = "HTML File (*.html)|*.html"
-    $saveFileDialog.ShowDialog() | Out-Null
-    $HtmlOutputPath = $saveFileDialog.FileName
-
-    if ($HtmlOutputPath -ne "") {
-        # Writing to HTML file
-        $htmlOutput | Out-File -FilePath $HtmlOutputPath -Encoding UTF8
-        Write-Host "Bookmarks saved to $HtmlOutputPath"
-    } else {
-        Write-Host "No file selected. Operation cancelled."
+    # Writing to HTML file
+    try {
+        $htmlOutput | Out-File -FilePath $outputPath -Encoding UTF8
+        Write-Host "Bookmarks saved to $outputPath"
+    } catch {
+        Write-Host "Failed to save the HTML file. Please check the output path and try again."
     }
 }
 
